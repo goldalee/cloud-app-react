6a0c029574abc66fad955cd0c11fa9a29c2fc679
goldalee <goldalee.sawyers@gmail.com>
2022-03-29 23:57:25 -0500

    Final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Support from './components/Support'
 import AllInOne from './components/AllInOne';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <>
@@ -11,6 +14,8 @@ function App() {
     <About />
     <Support />
     <AllInOne />
+    <Pricing />
+    <Footer />
 
     </>
   );
